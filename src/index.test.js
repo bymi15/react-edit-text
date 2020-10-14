6b3964de55280a0b19855561617d5216b32fd56c
Brian Min <bymi15@yahoo.com>
2020-10-14 02:18:33 +0100

    test: add basic unit test

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,14 @@
-import { ExampleComponent } from '.'
+import React from 'react';
+import EditText from '.';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
-  })
-})
+describe('EditText', () => {
+  it('clicking on the component activates edit mode', () => {
+    const component = mount(<EditText />);
+    expect(component.state().editMode).toEqual(false);
+    component.simulate('click');
+    expect(component.state().editMode).toEqual(true);
+  });
+});
