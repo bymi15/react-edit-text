7dae14f4e3065ed7c818a3203f86985edc2c133b
Brian Min <bymi15@yahoo.com>
2020-11-05 23:02:21 +0000

    docs(example): fix navbar home button

--- example/src/components/Navbar.js
+++ example/src/components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => (
     <div className='collapse navbar-collapse' id='navbarColor01'>
       <ul className='navbar-nav mr-auto'>
         <li className='nav-item active'>
-          <a className='nav-link' href='/'>
+          <a className='nav-link' href='https://brianmin.tech/react-edit-text/'>
             Home <span className='sr-only'>(current)</span>
           </a>
         </li>
